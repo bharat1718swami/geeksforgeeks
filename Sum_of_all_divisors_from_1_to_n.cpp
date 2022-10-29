355b8b79458c1184735110a50d674769e85ea8da
Bharat Swami <79807425+bharat1718swami@users.noreply.github.com>
2022-10-29 16:23:24 +0530

    Create Sum_of_all_divisors_from_1_to_n.cpp

---
+++ Sum_of_all_divisors_from_1_to_n.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+class Solution
+{
+public:
+
+    long long sumOfDivisors(int N)
+    {
+        
+        long long sum = 0;
+    for (int i = 1; i <= N; i++)
+        sum += (N / i) * i;
+    return sum;
+    
+
+    }
+};
