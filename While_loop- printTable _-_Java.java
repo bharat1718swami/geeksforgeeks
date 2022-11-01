ff8befbd150dc032e70c3397299d454c1b4532c4
Bharat Swami <79807425+bharat1718swami@users.noreply.github.com>
2022-11-01 16:59:36 +0530

    Create While_loop- printTable _-_Java.java

---
+++ While_loop- printTable _-_Java.java
@@ -0,0 +1,14 @@
+
+class Geeks {
+	 static void printTable (int n) 
+	 {
+            int multiplier=10;
+            while(multiplier>0)
+            {
+                System.out.print((multiplier--)*n+" ");
+                
+            }
+       System.out.println();
+	 }
+
+}
