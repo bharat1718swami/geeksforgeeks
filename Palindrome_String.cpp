463a580e86d4f5344a5b6fba4c3479d409fd5e6b
Bharat Swami <79807425+bharat1718swami@users.noreply.github.com>
2022-11-04 00:56:08 +0530

    Easy

---
+++ Palindrome_String.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int n=S.length();
+	    for(int i=0;i<n/2;i++){
+	        if(S[i]!=S[n-i-1]) return 0;
+	    }
+	    
+	    return 1;
+	}
+
+};
